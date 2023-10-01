cb642df9be23fdd55aba3902cf534d96c4ab3da1
Zion <zion@drcorp.co.kr>
2023-10-01 14:38:20 +0900

    feat: 3-7평균값구하기에서 얼만큼 올랐네요!만 하면 됨

--- 레벨3/7. array, for 반복문 실령향상과제/script.js
+++ 레벨3/7. array, for 반복문 실령향상과제/script.js
@@ -22,7 +22,7 @@ function 이름찾기1() {
 
 이름찾기1();
 
-// 2. 이름찾기연습2 (반복문, 서영연습)
+// 1-2. 이름찾기연습2 (반복문, 서영연습)
 function 이름찾기2() {
   for (let i = 0; i < 출석부.length; i++) {
     if (출석부[i] == "서영") {
@@ -32,7 +32,7 @@ function 이름찾기2() {
 }
 이름찾기2();
 
-// 3. 이렇게 해도 됨, name으로(코딩애플)
+// 1-3. 이렇게 해도 됨, name으로(코딩애플)
 function 이름찾기(name) {
   for (var i = 0; i < 4; i++) {
     if (name == 출석부[i]) {
@@ -47,3 +47,76 @@ function 이름찾기(name) {
 
 // 물론 indexOf, find 이런 자바스크립트 기본함수 찾아서 쓰면 array에서 문자검색을 쉽게 할 수 있습니다.
 // 그 기능을 직접 기본 문법만으로 짜는 것도 좋은 연습입니다.
+
+// 2. 구구단 콘솔에 출력하기(2단,3단,4단)
+for (let i = 1; i < 10; i++) {
+  if (i < 10) {
+    console.log(2 * i);
+  }
+}
+// if 없어도 됨
+for (let i = 1; i < 10; i++) {
+  console.log(2 * i);
+}
+for (let i = 1; i < 10; i++) {
+  console.log(3 * i);
+}
+for (let i = 1; i < 10; i++) {
+  console.log(4 * i);
+}
+
+// 1-9단까지 이중 for문
+for (let k = 1; k < 10; k++) {
+  console.log(k + "단");
+  for (let i = 1; i < 10; i++) {
+    console.log(k * i);
+  }
+}
+
+// 3. 평균점수 계산기 만들기
+score = [10, 20, 30, 40, 50];
+
+/*
+function 함수(score, b){
+  score 안에 있는 숫자들 평균 내기
+  그걸 b랑 비교해서 b가 더 크면 console.log('올랐네요')
+  b가 더 작으면 console.log('재수추천')
+}
+*/
+
+// 3-2. 평균값 구하기(다 더하고, 나누기 5)
+function 함수() {
+  let result = 0;
+  result = result + score[0]; //10
+  result = result + score[1]; //30
+  result = result + score[2]; //60
+  result = result + score[3]; //100
+  result = result + score[4]; //150
+  console.log(result+"(1)");
+}
+함수();
+
+// 3-2. 더 효율적으로 평균값 구하기
+
+function 함수2(score, b) {
+  let result = 0; //result 정의 먼저 한 후 for문 돌리기
+  let val = 0
+
+  for (let i = 0; i < score.length; i++) {
+    result = result + score[i];
+    val = b-result;
+  }
+  
+  console.log(result + "(2)");
+
+  if(result / score.length < b){
+    console.log("평균보다"+val+"점이나 올랐네요")
+    console.log(val+"점");
+  } else{
+    console.log("평균보다 같거나 떨어졌네요")
+  }
+}
+// 함수2(); //함수(score, b) 필요한 인자를 전달해주어야 오류가 안뜸
+함수2([10,10,10],40); //평균 10, 점수 40
+함수2([20,20,20],20); //평균 20
+함수2([20,20,20],10); //평균 20
